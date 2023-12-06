a4b3f91d519230260c64083e489f23173dc0b555
ngmn31 <rotten.dignity@gmail.com>
2023-12-06 21:52:10 +0900

    apiの場所をindexに変更

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+const request = require('request');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async (req, res) => {
+request('https://yesno.wtf/api', function (error, response,body) {
+
+if (!error && response.statusCode == 200) {
+const data = JSON.parse(body);
+res.json(data);
+
+}
 });
+})
 
-module.exports = router;
+module.exports = router
